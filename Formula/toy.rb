bc57e741919aa834e5192ad0c810916f9dccd0bd
Benny Nazimov <benny.n@pinecone.io>
2024-08-07 00:45:14 +0300

    address lints

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -4,11 +4,11 @@
 class Toy < Formula
   include Language::Python::Virtualenv
 
-  desc ""
-  homepage ""
+  desc "Description of the formula"
+  homepage "https://github.com/benny-n/toy"
   url "https://github.com/benny-n/toy/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "f15847a40678dcfd547447948a8c772c2874fa9cdcc8cf448e2982aac720c0b6"
-  license ""
+  license "MIT"
 
   depends_on "python"
 
