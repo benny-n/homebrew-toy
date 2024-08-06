ea25e79ddaf852452abea87d241949fb1860d2c8
Benny Nazimov <benny.n@pinecone.io>
2024-08-07 01:02:47 +0300

    update-sha

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -4,7 +4,7 @@ class Toy < Formula
   desc "Description of the formula"
   homepage "https://github.com/benny-n/toy"
   url "https://github.com/benny-n/toy/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "f15847a40678dcfd547447948a8c772c2874fa9cdcc8cf448e2982aac720c0b6"
+  sha256 "b0c780da369b1fe80a70038c3e7d2f23525f9fecd06a78235b5c200f7ca7f277"
   license "MIT"
 
   depends_on "python"
