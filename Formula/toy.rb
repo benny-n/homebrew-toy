99e8b71d459833cefc973372e9ff1534fe84e686
Benny Nazimov <benny.n@pinecone.io>
2024-08-07 00:59:30 +0300

    bump

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -3,7 +3,7 @@ class Toy < Formula
 
   desc "Description of the formula"
   homepage "https://github.com/benny-n/toy"
-  url "https://github.com/benny-n/toy/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/benny-n/toy/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "f15847a40678dcfd547447948a8c772c2874fa9cdcc8cf448e2982aac720c0b6"
   license "MIT"
 
