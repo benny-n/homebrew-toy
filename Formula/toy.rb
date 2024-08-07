68e7bef3c651d77afd4c1e83d512210ef31cbc98
Benny Nazimov <66024037+benny-n@users.noreply.github.com>
2024-08-07 12:59:41 +0300

    maybe

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -4,7 +4,7 @@ class Toy < Formula
   desc "Description of the formula"
   homepage "https://github.com/benny-n/toy"
   url "ssh://git@github.com/benny-n/toy.git", tag: "v0.4.0"
-  sha256 "89b50060e3330b7aafa93e8e1f701d256e42712a2f6b949226bd76b8624b7e7e"
+  # sha256 "89b50060e3330b7aafa93e8e1f701d256e42712a2f6b949226bd76b8624b7e7e"
   license "MIT"
 
   depends_on "python"
